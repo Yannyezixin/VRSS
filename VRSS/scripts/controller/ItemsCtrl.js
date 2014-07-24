c608b8d175fb6064dc92f9221019d80ee3524ec9
Yannyezixin <646840080@qq.com>
2014-07-24 10:43:58 +0800

    添加loading旋转gif

--- VRSS/scripts/controller/ItemsCtrl.js
+++ VRSS/scripts/controller/ItemsCtrl.js
@@ -5,5 +5,8 @@ angular.module('yann.MiniRSS')
         FeedLoad.fetch({q: feed.url, num: 100}, {}, function (data) {
             $scope.feed = data.responseData.feed;
             $scope.feed.id = feed.id;
+            $scope.loading = false;
         });
+
+        $scope.loading = true;
     });
