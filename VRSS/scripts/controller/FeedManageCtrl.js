546fed97cf49d2d65e94b3fd36053ee6b899417d
Yannyezixin <646840080@qq.com>
2014-07-24 11:01:44 +0800

    add loading when search a Url

--- VRSS/scripts/controller/FeedManageCtrl.js
+++ VRSS/scripts/controller/FeedManageCtrl.js
@@ -1,6 +1,7 @@
 angular.module('yann.MiniRSS')
     .controller('FeedManageCtrl', function ($scope, $location,UrlLookup, FeedLoad, FeedList) {
         $scope.feeds = FeedList.get();
+        $scope.searchLoading = false;
 
         $scope.add = function () {
             FeedList.add($scope.feed.url, $scope.feed.title);
@@ -12,8 +13,10 @@ angular.module('yann.MiniRSS')
         };
 
         $scope.lookup = function () {
+            $scope.searchLoading = true;
             UrlLookup.fetch({q: $scope.lookup.url}, {}, function (data) {
                 if (data.responseStatus != 200 || (data.responseData && data.responseData.url == '')) {
+                    $scope.searchLoading = false;
                     alert(data.responseDetails || 'Feed not found');
                     return;
                 }
@@ -21,11 +24,14 @@ angular.module('yann.MiniRSS')
                 $scope.feed = data.responseData;
                 FeedLoad.fetch({q: data.responseData.url},{}, function (data) {
                     if (data.responseStatus != 200) {
+                        $scope.searchLoading = false;
+                        alert('failed');
                         return;
                     }
 
                     $scope.feed.title = data.responseData.feed.title;
                 });
+                $scope.searchLoading = false;
             });
         };
 
