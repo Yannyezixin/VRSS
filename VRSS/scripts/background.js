e87c6065825853a4ff7c40ceb1d222ae0d3f87e7
Yannyezixin <646840080@qq.com>
2014-07-24 20:14:28 +0800

    background数据请求，返回响应

--- VRSS/scripts/background.js
+++ VRSS/scripts/background.js
@@ -8,20 +8,6 @@ function httpRequest(url, callback) {
     }
     xhr.send();
 }
-function FeedListAdd (result)
-{
-        var data = JSON.parse(result);
-        var title = data.responseData.feed.title;
-        var list = FeedListGet();
-        var id = localStorage.getItem('FeedListId') ? localStorage.getItem('FeedListId') : 1;
-        list.push({
-            url:    rssUrl,
-            title:  title,
-            id:     id
-        });
-        LocalObjectStorage.setObject('FeedList', list);
-        localStorage.setItem('FeedListId', ++id);
-}
 function FeedListGet () {
         if (LocalObjectStorage.contains('FeedList')) {
             return LocalObjectStorage.getObject('FeedList');
@@ -38,6 +24,34 @@ LocalObjectStorage.setObject = function (key, value) { localStorage.setItem(key,
 LocalObjectStorage.contains = function (key) { return localStorage.getItem(key) ? true : false;};
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+
+function addFeed(result) {
+    var data = JSON.parse(result);
+    rssUrl = data.responseData.url;
+    Url = 'http://ajax.googleapis.com/ajax/services/feed/load?callback?&num=1&q='+ rssUrl + '&v=1.0';
+    httpRequest(Url,FeedListAdd);
+}
+function FeedListAdd (result)
+{
+        var data = JSON.parse(result);
+        var title = data.responseData.feed.title;
+        var list = FeedListGet();
+        for (var i = list.length - 1; i >= 0; i--) {
+            if(list[i].url == rssUrl) {
+                sendResponse('success');
+                return;
+            }
+        }
+        var id = localStorage.getItem('FeedListId') ? localStorage.getItem('FeedListId') : 1;
+        list.push({
+            url:    rssUrl,
+            title:  title,
+            id:     id
+        });
+        LocalObjectStorage.setObject('FeedList', list);
+        localStorage.setItem('FeedListId', ++id);
+        sendResponse('success');
+}
     if (message == 'Add') {
         chrome.tabs.getSelected(null,function(tab){
             rssUrl = tab.url;
@@ -45,10 +59,35 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
             httpRequest(searchUrl,addFeed);
         });
     }
+    return true;
 });
-function addFeed(result) {
+
+function checkIfHasFeed (result) {
     var data = JSON.parse(result);
-    rssUrl = data.responseData.url;
-    Url = 'http://ajax.googleapis.com/ajax/services/feed/load?callback?&num=1&q='+ rssUrl + '&v=1.0';
-    httpRequest(Url,FeedListAdd);
+    if( data.responseStatus != 200) {
+        FeedHas = 'canAdd';
+        return;
+    }
+    var checkUrl = data.responseData.url;
+    var list = FeedListGet();
+    for (var i = list.length - 1; i >= 0; i--) {
+        if(list[i].url == checkUrl) {
+            FeedHas = 'hadAdd';
+            return;
+        }
+    }
+    FeedHas = 'NoAdd';
 }
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        var url = tab.url;
+        var searchUrl = 'http://ajax.googleapis.com/ajax/services/feed/lookup?callback?&q=' + url + '&v=1.0';
+        httpRequest(searchUrl, checkIfHasFeed);
+        chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+            if (message == 'FeedSame') {
+                sendResponse(FeedHas);
+            }
+            return true;
+        });
+    });
+});
