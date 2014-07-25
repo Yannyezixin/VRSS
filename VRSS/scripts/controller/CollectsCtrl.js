dcbc302fcfee7e9a8e9a8e468682a205d931c973
Yannyezixin <646840080@qq.com>
2014-07-25 19:34:26 +0800

    添加删除功能

--- VRSS/scripts/controller/CollectsCtrl.js
+++ VRSS/scripts/controller/CollectsCtrl.js
@@ -1,4 +1,12 @@
 angular.module('yann.MiniRSS')
     .controller('CollectsCtrl', function ($scope, CollectList) {
         $scope.collects = CollectList.get();
+
+        $scope.delete = function (id) {
+            CollectList.delete(id);
+        };
+
+        $scope.$on('CollectList', function (event, data) {
+            $scope.collects = data;
+        })
     });
